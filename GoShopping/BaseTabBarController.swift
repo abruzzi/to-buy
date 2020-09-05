454a520a66f3067a4a12c731c2b6ffd60d134367
Juntao Qiu <juntao.qiu@gmail.com>
2020-09-05 16:06:24 +1000

    badge

--- GoShopping/BaseTabBarController.swift
+++ GoShopping/BaseTabBarController.swift
@@ -12,17 +12,4 @@ class BaseTabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-    
-    func updateBadges() {
-        let allItems = fetchAllToBuyList()
-        let toBuyItems = allItems.filter { !$0.isCompleted && !$0.isDelayed }
-        let delayedItems = allItems.filter { $0.isDelayed }
-        
-        if let items = self.tabBarController?.tabBar.items as NSArray? {
-            let toBuyTab = items.object(at: 1) as! UITabBarItem
-            let delayedTab = items.object(at: 2) as! UITabBarItem
-            toBuyTab.badgeValue = String(toBuyItems.count)
-            delayedTab.badgeValue = String(delayedItems.count)
-        }
-    }
 }
