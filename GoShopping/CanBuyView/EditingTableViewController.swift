6d30ce04a509862f284a078460cb24fd29e833e6
Juntao Qiu <juntao.qiu@gmail.com>
2020-10-10 08:29:13 +1100

    fixed keyboard overlapping input issue

--- GoShopping/CanBuyView/EditingTableViewController.swift
+++ GoShopping/CanBuyView/EditingTableViewController.swift
@@ -87,8 +87,30 @@ class EditingTableViewController: UITableViewController, UIImagePickerController
         } ?? []
         
         supermarketTextField.filterStrings(options.compactMap { $0 })
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:)), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
     }
     
+    deinit {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+    }
+    
+    @objc func keyboardWillChange(notification: NSNotification) {
+        guard let keyboardRect = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else {
+            return
+        }
+        
+        if notification.name == UIResponder.keyboardWillChangeFrameNotification ||
+            notification.name == UIResponder.keyboardWillShowNotification {
+            view.frame.origin.y = -(keyboardRect.height)
+        } else {
+            view.frame.origin.y = 0
+        }
+    }
     
     //Action
     @objc func selectImage() {
--- GoShopping/ToBuyView/ToBuyItemTableViewController.swift
+++ GoShopping/ToBuyView/ToBuyItemTableViewController.swift
@@ -110,6 +110,8 @@ class ToBuyItemTableViewController: UITableViewController, UIImagePickerControll
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        supermarketTextField.delegate = self
+        
         self.tableView.keyboardDismissMode = .onDrag
         itemNameTextField.text = item.name
         supermarketTextField.text = item.supermarket
@@ -128,8 +130,30 @@ class ToBuyItemTableViewController: UITableViewController, UIImagePickerControll
         } ?? []
         
         supermarketTextField.filterStrings(options.compactMap { $0 })
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:)), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
     }
     
+    deinit {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+    }
+    
+    @objc func keyboardWillChange(notification: NSNotification) {
+        guard let keyboardRect = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else {
+            return
+        }
+        
+        if notification.name == UIResponder.keyboardWillChangeFrameNotification ||
+            notification.name == UIResponder.keyboardWillShowNotification {
+            view.frame.origin.y = -(keyboardRect.height)
+        } else {
+            view.frame.origin.y = 0
+        }
+    }
     
     //Action
     @objc func selectImage() {
@@ -170,5 +194,9 @@ class ToBuyItemTableViewController: UITableViewController, UIImagePickerControll
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         picker.dismiss(animated: true, completion: nil)
     }
+    
+}
+
+extension ToBuyItemTableViewController: UITextFieldDelegate {
     
 }
